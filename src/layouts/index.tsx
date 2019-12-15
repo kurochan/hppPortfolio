a19795309a46ef669d3450c9e447beaa30515247
hppRC <hpp.ricecake@gmail.com>
2019-12-16 08:29:43 +0900

    add Layout components

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,3 +1,16 @@
-export * from './layout';
-export * from './header';
-export * from './footer';
+import React from 'react';
+import { ResetCSS, GlobalCSS } from '../components';
+import { StyledHeader as Header } from './Header';
+import { StyledFooter as Footer } from './Footer';
+
+export const Layout: React.FC = ({ children }) => (
+  <>
+    <ResetCSS />
+    <GlobalCSS />
+    <Header />
+    {children}
+    <Footer />
+  </>
+);
+
+export default Layout;
